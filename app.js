e5fa8280f4e222f164ec44c7e06e9a741dd4b51f
thebigbillz <adenugbafavour@gmail.com>
2018-09-06 15:25:54 +0100

    Adding Item to UI

--- app.js
+++ app.js
@@ -54,7 +54,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
      
@@ -67,6 +69,26 @@ var UIController = (function() {
             value: document.querySelector(DOMstrings.inputValue).value
             };
             
+        },
+
+        addListItem: function (obj, type) {
+
+            var html;
+            if (type === 'inc') {
+
+                element = DOMstrings.incomeContainer;
+                html= '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"> <div class="item__value">%value%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            } else if (type ==='exp') {
+                element = DOMstrings.expensesContainer;
+                html= '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div><div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete">  <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div> </div>';
+            }
+          
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+          
         },
 
         getDOMstrings : function() {
@@ -94,6 +116,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
         var input = UICtrl. getinput();
       var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      UICtrl.addListItem(newItem, input.type);
 
     };
 
