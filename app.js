2e9232b3a2431d067ce41a6cbae2b1b90d039821
thebigbillz <adenugbafavour@gmail.com>
2018-09-09 17:11:09 +0100

    Clearing Input fields

--- app.js
+++ app.js
@@ -91,6 +91,21 @@ var UIController = (function() {
           
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+
+        },
+
         getDOMstrings : function() {
             return DOMstrings;
         }
@@ -117,6 +132,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var input = UICtrl. getinput();
       var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
       UICtrl.addListItem(newItem, input.type);
+      UICtrl.clearFields();
 
     };
 
